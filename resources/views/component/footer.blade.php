22b046c32ee7c5ad85e53586ea4f990c6f2c8bf9
Rxak22 <maboisakurak77@gmail.com>
2025-12-08 09:19:13 +0700

    setup vercel inviroment

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="bg-secondary rounded-top p-3">
         <div class="row">
             <div class="col-12 col-sm-6 text-center text-sm-start">
-                &copy; <a href="#">RUPP System</a>, All Right Reserved. 
+                &copy; <a href="#">School Management System</a>, All Right Reserved. 
             </div>
             <div class="col-12 col-sm-6 text-center text-sm-end">
                 <br>Develop By: <a href="" target="_blank">Rxak</a>
